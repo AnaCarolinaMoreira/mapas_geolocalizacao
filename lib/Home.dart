3a4372f46f8704b08ef8803cf2d1972f49490b32
Ana Moreira <anacarolina35980@gmail.com>
2020-10-23 16:37:01 -0300

    marcadores

--- lib/Home.dart
+++ lib/Home.dart
@@ -10,9 +10,35 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   Completer<GoogleMapController> _controller = Completer();
+  Set<Marker> _marcadores = {};
 
   _onMapCreated(GoogleMapController googleMapController) {
     _controller.complete(googleMapController);
+  }
+
+  _movimentarCamera() async {
+    GoogleMapController googleMapController = await _controller.future;
+
+    googleMapController
+        .animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+      target: LatLng(-23.562436, -46.655005),
+      zoom: 16,
+      tilt: 0,
+      bearing: 45,
+    )));
+  }
+
+  _carregarMarcadores() {
+    Marker marcadorShopping = Marker(
+        markerId: MarkerId("marcador-shopping"),
+      position: LatLng(-23.563370,-46.652923),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _carregarMarcadores();
   }
 
   @override
@@ -20,15 +46,24 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Mapas e Geolocalização"),
+        centerTitle: true,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        child: Icon(Icons.done),
+        onPressed: _movimentarCamera,
       ),
       body: Container(
         child: GoogleMap(
-          mapType: MapType.satellite,
+          mapType: MapType.normal,
           initialCameraPosition: CameraPosition(
             target: LatLng(-23.562436, -46.655005),
             zoom: 16,
           ),
           onMapCreated: _onMapCreated,
+          //exibe marcadores no mapa definidos
+          markers: _marcadores,
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:mapas_geolocalizacao/teste/teste_camadas.mapa.dart';
+
+import 'Home.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Mapas e geolocalização",
-    home: MapsTesteCamadas(),
+    home: Home(),
   ));
 }
