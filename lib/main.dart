f5db3c1e1c22f536eecf636b9d94727ce7233938
Ana Moreira <anacarolina35980@gmail.com>
2020-10-23 14:16:59 -0300

    inserção muda_cor_container.dart, teste_camadas.dart, layout de sobrepoisçõa redefinido

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mapas_geolocalizacao/teste/teste_camadas.mapa.dart';
 
-import 'Home.dart';
-
 void main() {
   runApp(MaterialApp(
     title: "Mapas e geolocalização",
